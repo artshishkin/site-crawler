faf83f87053561260f4b002c54dabf47b58e0227
Art <d.art.shishkin@gmail.com>
2022-09-27 17:11:26 +0300

    Added more logging

--- src/main/java/net/shyshkin/war/sitecrawler/service/WebClientFetchService.java
+++ src/main/java/net/shyshkin/war/sitecrawler/service/WebClientFetchService.java
@@ -1,6 +1,7 @@
 package net.shyshkin.war.sitecrawler.service;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.web.reactive.function.client.WebClient;
@@ -10,6 +11,7 @@ import reactor.core.publisher.Mono;
 import java.net.URI;
 import java.nio.charset.StandardCharsets;
 
+@Slf4j
 @Service
 @RequiredArgsConstructor
 public class WebClientFetchService implements FetchService {
@@ -23,9 +25,15 @@ public class WebClientFetchService implements FetchService {
     public Mono<String> fetchPage(String reservistName) {
         String encode = UriUtils.encode(reservistName, StandardCharsets.UTF_8);
         URI uri = URI.create(webSiteUrlPattern.replace("{reservist_name}", encode));
+        log.debug("Fetching {}", uri);
         return webClient
                 .get().uri(uri)
-                .retrieve()
-                .bodyToMono(String.class);
+                .exchangeToMono(clientResponse -> {
+                    log.debug("Status code: {}", clientResponse.statusCode());
+                    log.debug("Headers as HttpHeaders: {}", clientResponse.headers().asHttpHeaders());
+                    return clientResponse.bodyToMono(String.class);
+                })
+                .doOnNext(body -> log.debug("Response body: {}", body))
+                .log();
     }
 }
