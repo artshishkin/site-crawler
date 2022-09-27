ded80ad55e2c0b4704dbf09bcd48f0ecd0f32d50
Art <d.art.shishkin@gmail.com>
2022-09-27 14:09:41 +0300

    Init project

---
+++ src/main/java/net/shyshkin/war/sitecrawler/SiteCrawlerApplication.java
@@ -0,0 +1,13 @@
+package net.shyshkin.war.sitecrawler;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SiteCrawlerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SiteCrawlerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/net/shyshkin/war/sitecrawler/SiteCrawlerApplicationTests.java
@@ -0,0 +1,13 @@
+package net.shyshkin.war.sitecrawler;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SiteCrawlerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
