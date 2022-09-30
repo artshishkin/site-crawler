dcbb2dac13ab316bbf1e3e48394a5243f801db00
Art <d.art.shishkin@gmail.com>
2022-09-30 09:52:11 +0300

    1.8 Get Users API Unit Test (#1)

--- src/test/java/net/shyshkin/war/sitecrawler/api/ProxyControllerTest.java
+++ src/test/java/net/shyshkin/war/sitecrawler/api/ProxyControllerTest.java
@@ -1,6 +1,7 @@
 package net.shyshkin.war.sitecrawler.api;
 
 import net.shyshkin.war.sitecrawler.dto.SearchRequest;
+import net.shyshkin.war.sitecrawler.dto.VkUser;
 import net.shyshkin.war.sitecrawler.service.FetchService;
 import net.shyshkin.war.sitecrawler.service.VkApiService;
 import org.junit.jupiter.api.Test;
@@ -74,4 +75,73 @@ class ProxyControllerTest {
 
         then(fetchService).should().fetchSearchPage(eq(expectedSearchRequest));
     }
+
+    @Test
+    void getUser_withAcceptTxtHtml_shouldCallFetchService() {
+
+        //given
+        given(fetchService.fetchUserPage(anyString()))
+                .willReturn(Mono.just("Text HTML"));
+
+        //when
+        webTestClient
+                .get().uri("/users/12345678")
+                .accept(MediaType.TEXT_HTML)
+                .exchange()
+
+                //then
+                .expectStatus().isOk()
+                .expectBody(String.class)
+                .isEqualTo("Text HTML");
+
+        then(fetchService).should().fetchUserPage(eq("12345678"));
+    }
+
+    @Test
+    void getUser_withAcceptJson_shouldCallVkApiService() {
+
+        //given
+        VkUser expectedUser = new VkUser();
+        expectedUser.setId(12345678L);
+        expectedUser.setFirstName("Art");
+        expectedUser.setLastName("Shyshkin");
+        given(vkApiService.getUser(anyLong()))
+                .willReturn(Mono.just(expectedUser));
+
+        //when
+        webTestClient
+                .get().uri("/users/12345678")
+                .accept(MediaType.APPLICATION_JSON)
+                .exchange()
+
+                //then
+                .expectStatus().isOk()
+                .expectBody(VkUser.class)
+                .isEqualTo(expectedUser);
+
+        then(vkApiService).should().getUser(eq(12345678L));
+    }
+
+    @Test
+    void getUser_withAcceptJson_andParameterDebug_shouldCallVkApiService_fullJson() {
+
+        //given
+        String expectedJson = "{\"content\": \"full JSON\"}";
+        given(vkApiService.getUserJson(anyLong()))
+                .willReturn(Mono.just(expectedJson));
+
+        //when
+        webTestClient
+                .get().uri("/users/12345678?debug")
+                .accept(MediaType.APPLICATION_JSON)
+                .exchange()
+
+                //then
+                .expectStatus().isOk()
+                .expectBody(String.class)
+                .isEqualTo(expectedJson);
+
+        then(vkApiService).should().getUserJson(eq(12345678L));
+    }
+
 }
