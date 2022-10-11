fd867cbfe0a037d54ea5358951b329a7bcc54162
Art <d.art.shishkin@gmail.com>
2022-10-11 20:12:27 +0300

    8.4 Fix UserFields binding (#8)

--- vk-rest-api/src/main/java/net/shyshkin/war/vkrestapi/service/VkApiServiceImpl.java
+++ vk-rest-api/src/main/java/net/shyshkin/war/vkrestapi/service/VkApiServiceImpl.java
@@ -1,5 +1,6 @@
 package net.shyshkin.war.vkrestapi.service;
 
+import com.google.gson.Gson;
 import com.vk.api.sdk.client.Lang;
 import com.vk.api.sdk.client.VkApiClient;
 import com.vk.api.sdk.client.actors.UserActor;
@@ -9,14 +10,17 @@ import com.vk.api.sdk.objects.users.Fields;
 import com.vk.api.sdk.objects.users.UserFull;
 import com.vk.api.sdk.objects.users.responses.GetResponse;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import net.shyshkin.war.vkrestapi.config.data.VkApiConfigData;
 import org.springframework.stereotype.Service;
 import reactor.core.publisher.Mono;
 import reactor.core.scheduler.Schedulers;
 
 import java.util.List;
+import java.util.Objects;
 import java.util.stream.Collectors;
 
+@Slf4j
 @Service
 @RequiredArgsConstructor
 public class VkApiServiceImpl implements VkApiService {
@@ -36,9 +40,11 @@ public class VkApiServiceImpl implements VkApiService {
 
     private List<GetResponse> getUsers(List<String> ids) {
         try {
+            Gson gson = new Gson();
             List<Fields> fields = vkApiConfigData.getFields()
                     .stream()
-                    .map(Fields::valueOf)
+                    .map(str -> gson.fromJson(str, Fields.class))
+                    .filter(Objects::nonNull)
                     .collect(Collectors.toList());
 
             return vk.users().get(actor)
