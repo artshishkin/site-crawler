4be8514706ce3b4a7075bf668389172bcdeb9f0d
Art <d.art.shishkin@gmail.com>
2022-10-12 13:21:27 +0300

    8.11 Get User with Full JSON response (#8)

--- vk-rest-api/src/main/java/net/shyshkin/war/vkrestapi/api/ProxyController.java
+++ vk-rest-api/src/main/java/net/shyshkin/war/vkrestapi/api/ProxyController.java
@@ -1,14 +1,12 @@
 package net.shyshkin.war.vkrestapi.api;
 
+import com.google.gson.JsonElement;
 import com.vk.api.sdk.objects.users.UserFull;
 import lombok.RequiredArgsConstructor;
 import net.shyshkin.war.vkrestapi.dto.SearchRequest;
 import net.shyshkin.war.vkrestapi.service.VkApiService;
 import org.springframework.http.MediaType;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
@@ -25,6 +23,15 @@ public class ProxyController {
     @GetMapping(value = "/users/{id}", produces = MediaType.APPLICATION_JSON_VALUE)
     public Mono<UserFull> getUser(@PathVariable("id") Integer userId) {
         return vkApiService.getUser(userId);
+    }
+
+    @GetMapping(
+            value = "/users/{id}",
+            produces = MediaType.APPLICATION_JSON_VALUE,
+            params = "debug"
+    )
+    public Mono<String> getUserJson(@PathVariable("id") Integer userId) {
+        return vkApiService.getUsersJson(List.of(userId));
     }
 
     @GetMapping(
