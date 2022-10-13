429b683ceff8de127d3ea5047cc8518a5b465bba
Art <d.art.shishkin@gmail.com>
2022-10-13 10:02:32 +0300

    9.1 Init module (#9 VK Streaming API Module)

---
+++ vk-streaming-api/src/main/java/net/shyshkin/war/vkstreamingapi/VkStreamingApiApplication.java
@@ -0,0 +1,13 @@
+package net.shyshkin.war.vkstreamingapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VkStreamingApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VkStreamingApiApplication.class, args);
+    }
+
+}
---
+++ vk-streaming-api/src/test/java/net/shyshkin/war/vkstreamingapi/VkStreamingApiApplicationTest.java
@@ -0,0 +1,13 @@
+package net.shyshkin.war.vkstreamingapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VkStreamingApiApplicationTest {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
